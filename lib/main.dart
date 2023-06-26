ad4e5c57cc0183e508179fb0cee7f387e2193808
Alan Chablé <1120030@alumno.um.edu.mx>
2023-06-26 10:30:56 -0600

    Colors, scroll, and more :)

--- lib/main.dart
+++ lib/main.dart
@@ -26,103 +26,133 @@ class _MyPageState extends State<MyPage> {
 
       ),
 
-      body: Center(
+      body: SingleChildScrollView(
 
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            // Botón 1
-
-            MaterialButton(
-
-              onPressed: 
+        child: Center(
+      
+          child: Column(
+      
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+      
+              SizedBox(height: 16.0),
               
-              (buttonStatus[0] == true) ? () => _buttonPressed(0) : null,
-              child: Text('Botón 1'),
+              // Botón 1
+      
+              MaterialButton(
+      
+                height: 100,
+                color: Colors.purpleAccent.withAlpha(255),
+                disabledColor: Colors.deepPurpleAccent.withAlpha(100),
+                onPressed: (buttonStatus[0] == true) ? () => _buttonPressed(0) : null,
+                child: Text('Botón 1'),
+      
+              ),
+      
+              SizedBox(height: 16.0),
+      
+              // Botón 2
+      
+              MaterialButton(
+      
+                height: 100,
+                color: Colors.purpleAccent.withAlpha(255),
+                disabledColor: Colors.deepPurpleAccent.withAlpha(100),
+                onPressed: (buttonStatus[1] == true) ? () => _buttonPressed(1) : null,
+                child: Text('Botón 2'),
+      
+              ),
+      
+              SizedBox(height: 16.0),
+      
+              // Botón 3
+      
+              MaterialButton(
+      
+                height: 100,
+                color: Colors.purpleAccent.withAlpha(255),
+                disabledColor: Colors.deepPurpleAccent.withAlpha(100),
+                onPressed: (buttonStatus[2] == true) ? () => _buttonPressed(2) : null,
+                child: Text('Botón 3'),
+      
+              ),
+      
+              SizedBox(height: 16.0),
+      
+              // Botón 4
+      
+              MaterialButton(
+      
+                height: 100,
+                color: Colors.purpleAccent.withAlpha(255),
+                disabledColor: Colors.deepPurpleAccent.withAlpha(100),
+                onPressed: (buttonStatus[3] == true) ? () => _buttonPressed(3) : null,
+                child: Text('Botón 4'),
+      
+              ),
+      
+              SizedBox(height: 16.0),
+      
+              // Botón 5
+      
+              MaterialButton(
+      
+                height: 100,
+                color: Colors.purpleAccent.withAlpha(255),
+                disabledColor: Colors.deepPurpleAccent.withAlpha(100),
+                onPressed: (buttonStatus[4] == true) ? () => _buttonPressed(4) : null,
+                child: Text('Botón 5'),
+      
+              ),
+      
+              SizedBox(height: 16.0),
+      
+              // Botón 6
+      
+              MaterialButton(
+      
+                height: 100,
+                color: Colors.purpleAccent.withAlpha(255),
+                disabledColor: Colors.deepPurpleAccent.withAlpha(100),
+                onPressed: (buttonStatus[5] == true) ? () => _buttonPressed(5) : null,
+                child: Text('Botón 6'),
+      
+              ),
+      
+              SizedBox(height: 16.0),
+      
+              // Botón 7
+      
+              MaterialButton(
+      
+                height: 100,
+                color: Colors.purpleAccent.withAlpha(255),
+                disabledColor: Colors.deepPurpleAccent.withAlpha(100),
+                onPressed: (buttonStatus[6] == true) ? () => _buttonPressed(6) : null,
+                child: Text('Botón 7'),
+      
+              ),
+      
+              SizedBox(height: 16.0),
+      
+              // Botón 8
+      
+              MaterialButton(
+      
+                height: 100,
+                color: Colors.purpleAccent.withAlpha(255),
+                disabledColor: Colors.deepPurpleAccent.withAlpha(100),
+                onPressed: (buttonStatus[7] == true) ? () => _buttonPressed(7) : null,
+                child: Text('Botón 8'),
+      
+              ),
 
-            ),
-
-            SizedBox(height: 16.0),
-
-            // Botón 2
-
-            MaterialButton(
-
-              onPressed: (buttonStatus[1] == true) ? () => _buttonPressed(1) : null,
-              child: Text('Botón 2'),
-
-            ),
-
-            SizedBox(height: 16.0),
-
-            // Botón 3
-
-            MaterialButton(
-
-              onPressed: (buttonStatus[2] == true) ? () => _buttonPressed(2) : null,
-              child: Text('Botón 3'),
-
-            ),
-
-            SizedBox(height: 16.0),
-
-            // Botón 4
-
-            MaterialButton(
-
-              onPressed: (buttonStatus[3] == true) ? () => _buttonPressed(3) : null,
-              child: Text('Botón 4'),
-
-            ),
-
-            SizedBox(height: 16.0),
-
-            // Botón 5
-
-            MaterialButton(
-
-              onPressed: (buttonStatus[4] == true) ? () => _buttonPressed(4) : null,
-              child: Text('Botón 5'),
-
-            ),
-
-            SizedBox(height: 16.0),
-
-            // Botón 6
-
-            MaterialButton(
-
-              onPressed: (buttonStatus[5] == true) ? () => _buttonPressed(5) : null,
-              child: Text('Botón 6'),
-
-            ),
-
-            SizedBox(height: 16.0),
-
-            // Botón 7
-
-            MaterialButton(
-
-              onPressed: (buttonStatus[6] == true) ? () => _buttonPressed(6) : null,
-              child: Text('Botón 7'),
-
-            ),
-
-            SizedBox(height: 16.0),
-
-            // Botón 8
-
-            MaterialButton(
-
-              onPressed: (buttonStatus[7] == true) ? () => _buttonPressed(7) : null,
-              child: Text('Botón 8'),
-
-            ),
-
-          ],
-
+              SizedBox(height: 16.0),
+      
+            ],
+      
+          ),
+      
         ),
 
       ),
